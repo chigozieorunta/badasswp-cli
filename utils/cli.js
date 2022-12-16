61acd4e43e8586873dc7ac5545618cbaa9df4b4c
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-12-16 18:56:25 +0100

    Added flags and commands

---
+++ utils/cli.js
@@ -0,0 +1,50 @@
+const meow = require('meow');
+const meowHelp = require('cli-meow-help');
+
+const flags = {
+	clear: {
+		type: `boolean`,
+		default: true,
+		alias: `c`,
+		desc: `Clear the console`
+	},
+	noClear: {
+		type: `boolean`,
+		default: false,
+		alias: `k`,
+		desc: `Don't clear the console`
+	},
+	debug: {
+		type: `boolean`,
+		default: false,
+		alias: `d`,
+		desc: `Print debug info`
+	},
+	version: {
+		type: `boolean`,
+		alias: `v`,
+		desc: `Print CLI version`
+	}
+};
+
+const commands = {
+	help: { desc: `Print help info` },
+	make: { desc: `Make WP Plugin, Post, MetaBox and more` },
+	serve: { desc: `Serve WP site using Docker` },
+	test: { desc: `Run WP unit tests` },
+};
+
+const helpText = meowHelp({
+	name: `badassWP`,
+	flags,
+	commands
+});
+
+const options = {
+	inferType: true,
+	description: false,
+	hardRejection: false,
+	flags
+};
+
+module.exports = meow(helpText, options);
